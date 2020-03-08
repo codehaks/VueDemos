90f030a450db1330edd1a0070c738234cee861a8
Hakim Ghods <ghods.hakim@hotmail.com>
2020-03-08 04:15:55 +0330

    Add project files.

---
+++ MyApp/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyApp.Data;
+
+namespace MyApp
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>
+                (options => options.UseSqlite("Data Source=app.sqlite"));
+
+            services.AddControllers();
+
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
+
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+
+            });
+        }
+    }
+}
